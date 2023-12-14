776676f3c7eabdfdf2e42339bbeb2477478b7b61
Ross Young <ryoung@quantium.com>
2023-12-15 00:29:00 +1100

    update solution for part 1 of day 13

--- scripts/2023/13_point_of_incidence.R
+++ scripts/2023/13_point_of_incidence.R
@@ -22,111 +22,99 @@ puzzle <- read_lines(file = paste0("data/202313.txt"))
 # start with puzzle input
 puzzle %>% 
   
-  # break into separate mirror pieces based on blank row ("")
+  # break into separate mirror pieces based on blank row ("") separator
   split(x = ., f = 1 + cumsum(. == "")) %>% 
   
-  # for each mirror, remove the blank row ("")
+  # for each mirror piece, remove the blank row ("")
   map(.f = function(x) {discard(.x = x, .p = function(y) {y == ""})}) %>% 
   
   # save list as variable
   force() -> lst_puz
   
 
-# start with list
-lst_puz %>% 
+# write function to tranpose mirror (turn columns into rows) so that we can check for dupes in the same way
+my_fun <- function(x) {
   
-  # modify list to turn columns into rows, so that we can run the same check function.
-  purrr::modify(
-    .f = ~ .x %>% 
-      # split into separate pieces, convert to matrix and transpose
-      str_split(pattern = "", simplify = TRUE) %>% t() %>% 
-      # concatenate elements by row back into a string
-      apply(MARGIN = 1, FUN = function(x) {paste0(x, collapse = "")})
-  ) %>% 
+  # split into separate pieces, convert to matrix and transpose
+  str_split(string = x, pattern = "", simplify = TRUE) %>% t() %>% 
+    
+    # concatenate elements by row back into a string
+    apply(MARGIN = 1, FUN = paste0, collapse = "")
   
-  # save transposed list as variable
-  force() -> lst_puz_t
-  
+}
+
 
 
 # PART ONE ----------------------------------------------------------------
 
-# run loop for each list
-list(
-  lst_puz,
-  lst_puz_t
+# start with puzzle list (containing every mirror piece)
+map_dbl(
+  .x = lst_puz,
+  .f = function(LST) {
+    
+    # loop once for the mirror piece, and once for the transposed mirror piece
+    imap_dbl(
+      .x = list(LST, my_fun(LST)),
+      .f = function(MIRROR, MIRROR_ID) {
+        
+        # start with mirror piece
+        MIRROR %>% 
+          
+          # run-length-encoding to find where there is a duplicated value
+          rleid() %>% 
+          
+          # convert duplicate values into TRUE/FALSE
+          duplicated() %>% 
+          
+          # return elements where the duplication starts
+          {which(.) - 1} %>% 
+          
+          # save output as variable
+          force() -> xx
+        
+        # get multiplier based on row or column match
+        yy <- if (MIRROR_ID == 1) {100} else {1}
+        
+        # if no matches are found then return 0
+        if (length(xx) == 0) {NA_real_} else {
+          
+          # loop over each possible match
+          map_dbl(
+            .x = xx,
+            .f = function(x) {
+              
+              {
+                # if match occurs before the midpoint, start from left, otherwise right
+                if (x < length(MIRROR) / 2) {
+                  all(MIRROR[seq(x)] == MIRROR[seq(2 * x, x + 1)])
+                } else {
+                  all(MIRROR[tail(seq(x), length(MIRROR) - x)] == MIRROR[seq(length(MIRROR), x + 1)])
+                }
+                
+              } %>% 
+                
+                # if match fulfils condition, then return the match value, otherwise NULL
+                {if (. == TRUE) {yy * x} else {NA_real_}}
+            }
+          ) %>% 
+            
+            # reduce duplicates
+            {if (all(is.na(.))) {NA_real_} else {max(., na.rm = TRUE)}}
+          
+        }
+      }
+    ) %>% 
+      
+      # remove NA
+      discard(.p = ~ is.na(.x))
+  }
 ) %>% 
   
-  # run loop over each mirror within list
-  map(
-    .f = function(LST) {
-      
-      # start with list element
-      LST %>% 
-        
-        # run-length-encoding to find where there is a duplicated value
-        map(.f = rleid) %>% 
-        # convert duplicate values into TRUE/FALSE
-        map(.f = duplicated) %>% 
-        # return elements where the duplication starts
-        map(.f = ~ which(.x) - 1) %>% 
-        # only keep elements that have at least one possible match
-        keep(.p = ~ length(.x) > 0) %>% 
-        
-        # for each mirror containing a match, run a loop
-        imap(
-          .f = function(x, y) {
-            
-            # extract mirror to examine
-            xm <- LST[[y]]
-            
-            # run loop over each possible match
-            map(
-              .x = x,
-              .f = function(z) {
-                
-                # if match occurs before the midpoint start from left, otherwise right
-                {
-                  if (z < length(xm) / 2) {
-                    all(xm[seq(z)] == xm[seq(2 * z, z + 1)])
-                  } else {
-                    all(xm[tail(seq(z), length(xm) - z)] == xm[seq(length(xm), z + 1)])
-                  }
-                } %>% 
-                  
-                  # if match fulfils condition, then return the match value, otherwise NULL
-                  {if (.) {z} else {NULL}}
-                
-              }
-            ) %>% 
-              
-              # add mirror ID back onto the output
-              purrr::set_names(nm = y)
-            
-          }
-        ) %>%
-        
-        # get all list elements onto the same level
-        flatten() %>% 
-        
-        # remove any list elements containing NULL
-        compact() %>% 
-        
-        # convert from list to vector
-        unlist() %>%
-        
-        # add values
-        sum()
-      
-    }
-  ) %>% 
-  
-  # convert into a vector with two numbers (horizontal matches and vertical matches)
-  # multiply horizontal matches by 100
-  {flatten_dbl(.) * c(100, 1)} %>% 
-  
-  # add values
-  sum()
+  # save as output
+  force() -> outpu
+
+# add values
+sum(output)
 
 
 
